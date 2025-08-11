3c49fb2d383d708362c4e2ed94b6302002a5d74c
smhmurat <smhmurat@gmail.com>
2025-08-11 16:08:50 +0300

    Detail and Edit view added.

---
+++ Scrumdinger/DetailEditView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailEditView.swift
+//  Scrumdinger
+//
+//  Created by Semih Murat on 11.08.2025.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailEditView()
+}
